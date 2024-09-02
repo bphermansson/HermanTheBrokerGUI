935649dcebb7c7e5199c35ef7c5931adf03354fc
Patrik Hermansson <hermansson.patrik@gmail.com>
2024-09-02 15:55:26 +0200

    Model fix.

--- HermanTheBrokerGUI/Models/Broker.cs
+++ HermanTheBrokerGUI/Models/Broker.cs
@@ -5,8 +5,8 @@ namespace HermanTheBrokerGUI.Models
 {
 	public class Broker : IdentityUser
 	{
-        public string Name { get; set; }
-		public long PhoneNumber { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public long PhoneNumber { get; set; }
 		public ICollection<House> Houses { get; set; }
 	}
 }
